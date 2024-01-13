6db4256912795598cf2917ebf5c048b08adf4d20
kimdaaa <90200130+kimdaaa@users.noreply.github.com>
2024-01-14 01:42:51 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import presence
 from os import system
-import rpc
-
+from player_stats import PlayerStats
+from rank import Rank
+from rpc import Rpc
+from content import Content
+from Request1 import Requests
+from States.coregame import Coregame
+from States.menu import Menu
+from States.pregame import Pregame
+from utils import utilities
+from presence import Presences
 system('cls') 
 system(f"title Loyal v1")
+
+Requests = Requests()
+content = Content(Requests)
+Pregame = Pregame(Requests)
+utilities = utilities(Requests)
+
+if __name__ == "__main__":
+    a = Pregame.get_pregame_match_id()
+    print(a)
+    utilities.dodge(a)
+
+
